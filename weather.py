dd564287a07ee8c72c1c92716df74e60951f5e72
dd.saushkin <dd.saushkin@tensor.ru>
2024-07-12 13:54:51 +0300

    initial commit

---
+++ weather.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city="Samara"):
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('API_KEY')}"
+    weather_data = requests.get(weather_url).json()
+
+    return weather_data
+
+
+if __name__ == "__main__":
+    print("\n*** Current weather ***\n")
+    city = input("\nPlease enter a city name: ")
+    # empty or spaces
+    if not city.strip():
+        city = "Moscow"
+
+    weather_data = get_current_weather(city)
+
+    print("\n")
+    pprint(weather_data)
